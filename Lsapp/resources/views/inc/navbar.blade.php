03b7076f5c52d86c8ac766f1f3deff5baae9d2ba
MbuguaCaleb <mbuguacaleb30@gmail.com>
2019-03-16 16:34:44 +0300

    [ft]-modified-layouts-after-auth-controller

--- Lsapp/resources/views/inc/navbar.blade.php
+++ Lsapp/resources/views/inc/navbar.blade.php
@@ -1,31 +1,53 @@
-<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+<nav class="navbar navbar-expand-md navbar-light navbar-dark bg-dark ">
+    <div class="container">
+        <a class="navbar-brand" href="{{ url('/') }}">
+            {{ config('app.name', 'Laravel') }}
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
+            <ul  class="navbar-nav mr-auto" >
+                <li><a class="nav-link"  href="/">Home</a></li>
+                <li><a class="nav-link"href="/about">About</a></li>
+                <li><a class="nav-link" href="/services">Services</a></li>
+                <li><a class="nav-link" href="/posts">Blog</a></li>
+              </ul>
 
-        <div class="container">
-                <div class="navbar-header">
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav ml-auto">
+                <!-- Authentication Links -->
+                @guest
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    </li>
+                    @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                    @endif
+                @else
+                    <li class="nav-item dropdown">
 
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
 
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="/dashboard">Dashboard</a>
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
 
-                    <ul  class="navbar-nav mr-auto" >
-                      <li><a class="nav-link"  href="/">Home</a></li>
-                      <li><a class="nav-link"href="/about">About</a></li>
-                      <li><a class="nav-link" href="/services">Services</a></li>
-                      <li><a class="nav-link" href="/posts">Blog</a></li>
-                    </ul>
-
-                    <ul class="nav navbar-nav  navbar-right" >
-                        <li><a class="nav-link" href="/posts/create">Create Post</a></li>
-                    </ul>
-                </div>
-
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                @endguest
+            </ul>
+        </div>
+    </div>
 </nav>
